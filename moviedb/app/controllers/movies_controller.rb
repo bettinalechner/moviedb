b77e018a16dad8db6f33eaf4e467275cd22a9483
Bettina Lechner <blechner@unomaha.edu>
2015-07-13 21:00:02 +0000

    create, show, index, and delete for movies

--- moviedb/app/controllers/movies_controller.rb
+++ moviedb/app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
     def index
+        @movies = Movie.all
+    end
+    
+    def show
+        @movie = Movie.find(params[:id])
     end
     
     def new
@@ -12,6 +17,13 @@ class MoviesController < ApplicationController
        @movie.save
        
        redirect_to @movie
+    end
+    
+    def destroy
+       @movie = Movie.find(params[:id])
+       @movie.destroy
+       
+       redirect_to movies_path
     end
     
     private
