e8e89333e9a2911f421e823fe2a9ba6446875ca2
Bettina Lechner <blechner@unomaha.edu>
2015-07-14 20:06:32 +0000

    add actor index and validation

--- moviedb/app/models/actor.rb
+++ moviedb/app/models/actor.rb
@@ -1,4 +1,10 @@
 class Actor < ActiveRecord::Base
+    
+    validates :first_name, presence: true, length: { minimum: 2 }
+    validates :last_name, presence: true, length: { minimum: 2 }
+    validates :nationality, presence: true, length: { minimum: 2 }
+    validates :date_of_birth, presence: true
+    
     def name
        self.first_name + ' ' + self.last_name
     end
