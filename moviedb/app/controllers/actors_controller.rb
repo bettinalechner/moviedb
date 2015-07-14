188d076b6b4f6bd9214fe6447b343f125ae8b2bb
Bettina Lechner <blechner@unomaha.edu>
2015-07-14 20:15:42 +0000

    actors edit and delete

--- moviedb/app/controllers/actors_controller.rb
+++ moviedb/app/controllers/actors_controller.rb
@@ -21,6 +21,27 @@ class ActorsController < ApplicationController
         end
     end
     
+    def edit
+        @actor = Actor.find(params[:id])
+    end
+    
+    def update
+        @actor = Actor.find(params[:id])
+        
+        if @actor.update(actor_params)
+            redirect_to @actor
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @actor = Actor.find(params[:id])
+        @actor.destroy
+        
+        redirect_to actors_path
+    end
+    
     private
     
     def actor_params
