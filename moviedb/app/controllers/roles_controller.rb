e42d7ace2ab7995b079f34ef75cc40de665d08de
Bettina Lechner <blechner@unomaha.edu>
2015-07-15 19:06:09 +0000

    finish out Role CRUD from movies, add roles from actors

--- moviedb/app/controllers/roles_controller.rb
+++ moviedb/app/controllers/roles_controller.rb
@@ -1,18 +1,59 @@
 class RolesController < ApplicationController
     def new
         @role = Role.new
-        @movie = Movie.find(params[:movie_id])
-        render 'new_role'
+        if params[:movie_id]
+            @movie = Movie.find(params[:movie_id])
+            render 'new_role'
+        elsif params[:actor_id]
+            @actor = Actor.find(params[:actor_id])
+            render 'new_cast'
+        else
+            raise 'Actor or Movie must be defined'
+        end
     end
     
     def create
-        @movie = Movie.find(params[:movie_id])
-        
-        if @movie.roles.create(role_params)
-            redirect_to @movie
+        if params[:movie_id]
+            @movie = Movie.find(params[:movie_id])
+            
+            if @movie.roles.create(role_params)
+                redirect_to @movie
+            else
+                render 'new_role'
+            end
+        elsif params[:actor_id]
+            @actor = Actor.find(params[:actor_id])
+            
+            if @actor.roles.create(role_params)
+                redirect_to @actor
+            else
+                render 'new_cast'
+            end
         else
-            render 'new_role'
+            raise 'Actor or Movie must be defined'
         end
+    end
+    
+    def edit
+        @role = Role.find(params[:id])
+        @movie = Movie.find(params[:movie_id])
+    end
+    
+    def update
+        @role = Role.find(params[:id])
+        if @role.update(role_params)
+            redirect_to @role.movie
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @role = Role.find(params[:id])
+        @movie = Movie.find(params[:movie_id])
+        @role.destroy
+        
+        redirect_to @movie
     end
     
     private
